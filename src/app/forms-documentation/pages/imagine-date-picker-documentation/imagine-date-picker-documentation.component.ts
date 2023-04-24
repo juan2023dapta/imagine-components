cad1df37c523209f7e109982693ca9ce7e91f63f
Juan_Rojas <juan.rojas@imagineapps.co>
2023-04-24 17:33:33 -0500

    datepicker for data ready

---
+++ src/app/forms-documentation/pages/imagine-date-picker-documentation/imagine-date-picker-documentation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-imagine-date-picker-documentation',
+  templateUrl: './imagine-date-picker-documentation.component.html',
+  styleUrls: ['./imagine-date-picker-documentation.component.scss']
+})
+export class ImagineDatePickerDocumentationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
